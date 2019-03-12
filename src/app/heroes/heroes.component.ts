3f9329186047e2932f07f91c692dd74bef708b07
Soulyana <soulyana.lakew@accenturefederal.com>
2019-03-12 15:15:24 -0400

    created click event handler to selectHero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,11 +15,15 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit() {
 
   }
-
 }
 
